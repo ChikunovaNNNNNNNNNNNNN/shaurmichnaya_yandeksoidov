64429b3d2dc574f2606beb5299ed1abb0e5b957b
msnastasia <anastasiasergeevna3010@gmail.com>
2025-01-26 17:53:26 +0300

    Update zhestokaya_bojnya.py

--- zhestokaya_bojnya.py
+++ zhestokaya_bojnya.py
@@ -1,12 +1,15 @@
 import sys
 import pygame
 import os
+import random
 
 
 pygame.init()
 pygame.display.set_caption('Шаурмичная яндексоидов')
 size = width, height = 800, 800
 screen = pygame.display.set_mode(size)
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
 
 
 def load_image(name, colorkey=None):
@@ -24,33 +27,82 @@ def load_image(name, colorkey=None):
         image = image.convert_alpha()
     return image
 
+tile_images = {
+        'bleu': pygame.transform.scale(load_image('blue.png'), (60, 60)),
+        'violet': pygame.transform.scale(load_image('violet.png'), (60, 60)),
+        'jaune': pygame.transform.scale(load_image('jaune.png'), (60, 60)),
+        'vert': pygame.transform.scale(load_image('vert.png'), (60, 60)),
+        'rouge': pygame.transform.scale(load_image('rouge.png'), (60, 60))}
 
-class MySprite(pygame.sprite.Sprite):
-    all_sprites = pygame.sprite.Group()
-    sprite = pygame.sprite.Sprite()
+tile_width = tile_height = 80
+
+class Hod:
+    ...
+
+
+class Bleu(pygame.sprite.Sprite):
     image1 = load_image("blue.png")
     image = pygame.transform.scale(image1, (60, 60))
-    image2 = load_image("violet.png")
-    imagea = pygame.transform.scale(image2, (60, 60))
-    image3 = load_image("jaune.png")
-    imageb = pygame.transform.scale(image3, (60, 60))
-    image4 = load_image("vert.png")
-    imagec = pygame.transform.scale(image4, (60, 60))
-    image5 = load_image("rouge.png")
-    imaged = pygame.transform.scale(image5, (60, 60))
 
 
     def __init__(self, *group):
         super().__init__(*group)
-        self.image = MySprite.image
+        self.image = Bleu.image
         self.rect = self.image.get_rect()
         self.rect.x = 210
-        self.rect.y = 90
-        self.imagea = MySprite.imagea
-        self.rect1 = self.imagea.get_rect()
-        self.rect1.x = 300
-        self.rect1.y = 90
+        self.rect.y = 130
 
+
+class Violet(pygame.sprite.Sprite):
+    image1 = load_image("violet.png")
+    image = pygame.transform.scale(image1, (60, 60))
+
+
+    def __init__(self, *group):
+        super().__init__(*group)
+        self.image = Violet.image
+        self.rect = self.image.get_rect()
+        self.rect.x = 290
+        self.rect.y = 130
+
+
+class Jaune(pygame.sprite.Sprite):
+    image1 = load_image("jaune.png")
+    image = pygame.transform.scale(image1, (60, 60))
+
+
+    def __init__(self, *group):
+        super().__init__(*group)
+        self.image = Jaune.image
+        self.rect = self.image.get_rect()
+        self.rect.x = 290 + 80
+        self.rect.y = 130
+
+
+class Vert(pygame.sprite.Sprite):
+    image1 = load_image("vert.png")
+    image = pygame.transform.scale(image1, (60, 60))
+
+
+    def __init__(self, *group):
+        super().__init__(*group)
+        self.image = Vert.image
+        self.rect = self.image.get_rect()
+        self.rect.x = 290 + 160
+        self.rect.y = 130
+
+
+class Rouge(pygame.sprite.Sprite):
+    image1 = load_image("rouge.png")
+    image = pygame.transform.scale(image1, (60, 60))
+
+
+    def __init__(self, *group):
+        super().__init__(*group)
+        self.image = Rouge.image
+        self.rect = self.image.get_rect()
+        self.rect.x = 290 + 160 + 80
+        self.rect.y = 130
 
 
 class Board:
@@ -61,7 +113,7 @@ class Board:
         self.left = 10
         self.top = 10
         self.cell_size = 30
-        self.color = pygame.Color(110, 110, 255)
+        self.color = pygame.Color(255, 110, 60)
         self.cell_x = self.cell_y = None
 
     def set_view(self, left, top, cell_size):
@@ -82,6 +134,7 @@ class Board:
             1] > self.top + self.cell_size * self.height):
             self.cell_x = self.cell_y = None
         self.print_click()
+        return self.cell_x, self.cell_y
 
     def print_click(self):
         if self.cell_x is None:
@@ -90,22 +143,108 @@ class Board:
             print(f"({self.cell_x}, {self.cell_y})")
 
 
+def load_level(filename):
+    filename = "фишки"
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    return list(level_map)
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+                tile_width * pos_x + 170, tile_height * pos_y + 220)
+
+
+def generate_couleurs(level):
+    blue = []
+    violet = []
+    jaune = []
+    vert = []
+    rouge =[]
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '*':
+                Tile('bleu', x, y)
+                blue.append((x, y))
+            elif level[y][x] == '.':
+                Tile('violet', x, y)
+                violet.append((x, y))
+            elif level[y][x] == '+':
+                Tile('jaune', x, y)
+                jaune.append((x, y))
+            elif level[y][x] == '-':
+                Tile('vert', x, y)
+                vert.append((x, y))
+            elif level[y][x] == '=':
+                Tile('rouge', x, y)
+                rouge.append((x, y))
+    return blue, violet, jaune, vert, rouge
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '*':
+                Tile('bleu', x, y)
+            elif level[y][x] == '.':
+                Tile('violet', x, y)
+            elif level[y][x] == '+':
+                Tile('jaune', x, y)
+            elif level[y][x] == '-':
+                Tile('vert', x, y)
+            elif level[y][x] == '=':
+                Tile('rouge', x, y)
+    return new_player, x, y
+
+player, level_x, level_y = generate_level(load_level('map.txt'))
+
+
+class Game:
+    def __init__(self, bleu, violet, jaune, vert, rouge):
+        self.bleu = generate_couleurs(bleu)
+        self.violet = generate_couleurs(violet)
+        self.jaune = generate_couleurs(jaune)
+        self.vert = generate_couleurs(vert)
+        self.rouge = generate_couleurs(rouge)
+
+
+    def get_position(self):
+        for cords in self.bleu:
+            ...
+
+    def place(self, b, v, j, ve, r):
+        print(b, v, j, ve, r, sep='\n')
+
+
+
 def main():
-    all_sprites = pygame.sprite.Group()
-    hero = MySprite(all_sprites)
+    bleu = Bleu(all_sprites)
+    violet = Violet(all_sprites)
+    jaune = Jaune(all_sprites)
+    vert = Vert(all_sprites)
+    rouge = Rouge(all_sprites)
+    game = Game
 
     fps = 60
     clock = pygame.time.Clock()
     board = Board(6, 6)
-    board.set_view(160, 170, 80)
+    board.set_view(160, 210, 80)
     running = True
+    background = pygame.image.load('фон.jpg').convert()
+    gameDisplay = pygame.display.set_mode(size)
+    background = pygame.transform.smoothscale(background, gameDisplay.get_size())
+    pygame.mixer.music.load("Магистр.mp3")
+    pygame.mixer.music.play(-1)
     while running:
-        screen.fill((240, 255, 240))
+        gameDisplay.blit(background, (0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                board.get_click(event.pos)
+                if event.button == 2:
+                    board.get_click(event.pos)
 
         all_sprites.draw(screen)
 
