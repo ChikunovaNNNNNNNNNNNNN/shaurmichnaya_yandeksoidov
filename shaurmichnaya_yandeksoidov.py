65d9af9212824008c64874a00d5e085f0fe758c6
Элементарно, Ватсон! <tapkoed15@gmail.com>
2025-02-03 19:27:49 +0300

    Add files via upload

---
+++ shaurmichnaya_yandeksoidov.py
@@ -0,0 +1,5 @@
+import subprocess
+subprocess.run('python q1.py', shell=True, check=False)
+subprocess.run('python q2.py', shell=True, check=False)
+subprocess.run('python q3.py', shell=True, check=False)
+subprocess.run('python red.py', shell=True, check=False)
